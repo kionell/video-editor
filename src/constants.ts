d1cf996ea478c470ae0da113301ad0c8ed2f3028
Alexander <sashakostyunin1@gmail.com>
2022-09-25 01:51:20 +0300

    Increase size of a normal icon

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const LARGE_ICON_SIZE = 32;
-export const NORMAL_ICON_SIZE = 16;
+export const NORMAL_ICON_SIZE = 20;
 
 export const SMALL_FONT_SIZE = 14;
 export const NORMAL_FONT_SIZE = 16;
