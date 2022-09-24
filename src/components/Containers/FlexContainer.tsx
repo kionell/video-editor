2d5d749efd81469eb2563561a77b628490231fa3
Alexander <sashakostyunin1@gmail.com>
2022-09-24 06:31:26 +0300

    Create flex container component

---
+++ src/components/Containers/FlexContainer.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+type FlexDirection = 'row' | 'column';
+type FlexJustifying = 'center' | 'space-between' | 'space-around';
+type FlexAligning = 'left' | 'center';
+
+interface FlexProps {
+  direction?: FlexDirection;
+  reversed?: boolean;
+  justify?: FlexJustifying;
+  align?: FlexAligning;
+}
+
+const StyledFlexContainer = styled.div<FlexProps>`
+  padding: 6px;
+  display: flex;
+  flex-direction: ${(props) => {
+    return props.direction + (props.reversed ? '-reverse' : '');
+  }};
+
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align};
+`;
+
+const FlexContainer: React.FC<PropsWithChildren<FlexProps>> = (props: FlexProps) => {
+  return <StyledFlexContainer {...props}></StyledFlexContainer>;
+};
+
+FlexContainer.defaultProps = {
+  direction: 'row',
+  reversed: false,
+  justify: 'center',
+  align: 'center',
+};
+
+export { FlexContainer };
