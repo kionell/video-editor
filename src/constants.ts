d3f3322274d6e8cbc049a6aa3f227d7a23c28e3e
Alexander <sashakostyunin1@gmail.com>
2022-09-30 17:50:37 +0300

    Add new constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
 export const LARGE_ICON_SIZE = 30;
 export const NORMAL_ICON_SIZE = 20;
+export const SMALL_ICON_SIZE = 15;
 
-export const SMALL_FONT_SIZE = 14;
 export const NORMAL_FONT_SIZE = 20;
+export const SMALL_FONT_SIZE = 14;
+
+export const DEFAULT_FONT = 'Roboto';
+export const DEFAULT_WEIGHT = 'Regular';
+
+export const SECONDARY_FONT = 'sans-serif';
