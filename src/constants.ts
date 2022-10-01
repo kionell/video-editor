2cca2339b662d6f13e75e19cb9a0c5c2df6789e9
Alexander <sashakostyunin1@gmail.com>
2022-10-01 05:23:31 +0300

    Reduce font & icon sizes

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const GIANT_ICON_SIZE = 100;
 export const LARGE_ICON_SIZE = 30;
 export const NORMAL_ICON_SIZE = 20;
-export const SMALL_ICON_SIZE = 15;
+export const SMALL_ICON_SIZE = 14;
 
-export const LARGE_FONT_SIZE = 28;
-export const NORMAL_FONT_SIZE = 18;
+export const LARGE_FONT_SIZE = 24;
+export const NORMAL_FONT_SIZE = 16;
 export const SMALL_FONT_SIZE = 12;
 
 export const DEFAULT_FONT = 'Roboto';
