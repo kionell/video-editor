4f931e65f81a4b1781192f1a28824c268ec4306e
Alexander <sashakostyunin1@gmail.com>
2022-10-03 02:42:15 +0300

    Implement simple timeline system

---
+++ src/models/Timeline/Timeline.ts
@@ -0,0 +1,151 @@
+import { AudioElement } from '../Elements/AudioElement';
+import { ImageElement } from '../Elements/ImageElement';
+import { TextElement } from '../Elements/TextElement';
+import { VideoElement } from '../Elements/VideoElement';
+import { MediaType } from '../Enums/MediaType';
+import { TimelineTrack } from './TimelineTrack';
+
+/**
+ * A timeline.
+ */
+export class Timeline {
+  private _tracks: TimelineTrack[] = [];
+	
+  /**
+   * Current time of the playback in milliseconds.
+   */
+  currentTimeMs = 0;
+
+  /**
+   * Current zoom of this timeline.
+   */
+  currentZoom = 1;
+
+  /**
+   * Current scroll position of this timeline.
+   */
+  currentScroll = 0;
+
+	/**
+   * Adds a new track to this timeline.
+   * @param track Track to add.
+   */
+  addTrack(track: TimelineTrack): void {
+    this._tracks.splice(track.index, 0, track);
+
+    this._reindexTracks();
+  }
+
+	/**
+   * Removes an existing track from this timeline.
+   * @param track Track to remove.
+   */
+	removeTrack(track: TimelineTrack): void {
+    const index = this._tracks.findIndex((t) => t === track);
+
+    return this.removeTrackByIndex(index);
+  }
+
+  /**
+   * Removes an existing track from this timeline by its index.
+   * @param index Target index of a track.
+   */
+  removeTrackByIndex(index: number): void {
+    if (index < 0 || index >= this.totalTracks) return;
+    
+    this._tracks.splice(index, 1);
+
+    this._reindexTracks();
+  }
+
+  /**
+   * Moves target track to a specific index.
+   * @param track Target track.
+   * @param toIndex Target index.
+   */
+  moveTrack(track: TimelineTrack, toIndex: number): void {
+    const fromIndex = this._tracks.findIndex((t) => t === track);
+
+    return this.moveTrackByIndex(fromIndex, toIndex);
+  }
+
+  /**
+   * Moves an existing track from one index to another.
+   * @param fromIndex Source index of a track.
+   * @param toIndex Target index of a track.
+   */
+  moveTrackByIndex(fromIndex: number, toIndex: number): void {
+    if (fromIndex < 0 || fromIndex >= this.totalTracks) return;
+
+    const track = this._tracks.splice(fromIndex, 1);
+    const index = Math.max(0, Math.min(toIndex, this.totalLength - 1));
+
+    this._tracks.splice(index, 0, track[0]);
+
+    this._reindexTracks();
+  }
+
+  /**
+   * Searches for a track by its index.
+   * @param index Target index of a track.
+   * @returns Found timeline track or null.
+   */
+  getTrackByIndex(index: number): TimelineTrack | null {
+    if (index < 0 || index >= this.totalTracks) {
+      return null;
+    }
+
+    return this._tracks[index];
+  }
+
+  get totalTracks(): number {
+    return this._tracks.length;
+  }
+
+  get videoTracks(): TimelineTrack<VideoElement>[] {
+    const result = this._tracks.filter((t) => t.type === MediaType.Video);
+    
+    return result as TimelineTrack<VideoElement>[];
+  }
+
+	get audioTracks(): TimelineTrack<AudioElement>[] {
+    const result = this._tracks.filter((t) => t.type === MediaType.Audio);
+    
+    return result as TimelineTrack<AudioElement>[];
+  }
+
+	get imageTracks(): TimelineTrack<ImageElement>[] {
+    const result = this._tracks.filter((t) => t.type === MediaType.Image);
+    
+    return result as TimelineTrack<ImageElement>[];
+  }
+
+	get textTracks(): TimelineTrack<TextElement>[] {
+    const result = this._tracks.filter((t) => t.type === MediaType.Text);
+    
+    return result as TimelineTrack<TextElement>[];
+  }
+
+  get startTimeMs(): number {
+    return this._tracks.reduce((ms, t) => Math.min(ms, t.startTimeMs), Infinity);
+  }
+
+	get endTimeMs(): number {
+    return this._tracks.reduce((ms, t) => Math.max(ms, t.endTimeMs), -Infinity);
+  }
+
+	get durationMs(): number {
+    return this.endTimeMs - this.startTimeMs;
+  }
+
+  get totalLength(): number {
+    return this.endTimeMs;
+  }
+
+  /**
+   * Fixes order of all track indexes.
+   */
+  private _reindexTracks(): void {
+    this._tracks.forEach((track, index) => track.index = index);
+  }
+}
---
+++ src/models/Timeline/TimelineTrack.ts
@@ -0,0 +1,145 @@
+import { findIndex } from '../../utils';
+import { BaseElement } from '../Elements/BaseElement';
+import { MediaType } from '../Enums/MediaType';
+
+/**
+ * A timeline track.
+ */
+export class TimelineTrack<T extends BaseElement = BaseElement> {
+	/**
+   * Index of timeline track on the timeline track area.
+   */
+  index: number;
+	
+  /**
+   * Type of this media.
+   */
+  type: MediaType;
+
+  private _elements: T[] = [];
+
+  constructor(index: number, type: MediaType) {
+    this.index = index;
+    this.type = type;
+  }
+
+	/**
+   * Adds a new element to this timeline track.
+   * @param element Element to add.
+   */
+	addElement(element: T): void {
+    this._elements.push(element);
+    
+    this._fixTimeOffsets();
+  }
+
+	/**
+   * Removes an existing element from this timeline track.
+   * @param element Element to remove.
+   */
+	removeElement(element: T): void {
+    const index = this._elements.findIndex((el) => el === element);
+
+    this._elements.splice(index, 1);
+  }
+
+  /**
+   * Removes an existing element from this timeline track by current time.
+   * @param time Current time of the track.
+   */
+  removeElementAtTime(time: number): void {
+    const element = this.getElementAtTime(time);
+
+    if (element) this.removeElement(element);
+  }
+	
+  /**
+   * Searches for an element which is played at the specified time.
+   * @param time Time in milliseconds.
+   * @returns Found element or null.
+   */
+	getElementAtTime(time: number): T | null {
+    const predicate = (element: T) => {
+      return element.startTimeMs >= time && element.endTimeMs <= time;
+    };
+
+    const index = findIndex(this._elements, predicate);
+
+    return index >= 0 ? this._elements[index] : null;
+  }
+
+  /**
+   * Moves target element to another point of time.
+   * @param element Target element.
+   * @param toMs Target time.
+   */
+  moveElementToTime(element: T, toMs: number): void {
+    element.offsetMs = Math.max(0, toMs);
+
+    this._fixTimeOffsets();
+  }
+
+  /**
+   * Moves an existing element from one point of time to another.
+   * @param fromMs Source time.
+   * @param toMs Target time.
+   */
+  moveElementFromTimeToTime(fromMs: number, toMs: number): void {
+    // There are no elements outside of track's total length.
+    if (fromMs < 0 || fromMs > this.totalLength) return;
+
+    const element = this.getElementAtTime(fromMs);
+
+    if (!element) return;
+
+    this.moveElementToTime(element, toMs);
+  }
+
+  get firstElement(): T | null {
+    return this._elements[0] ?? null;
+  }
+
+  get lastElement(): T | null {
+    return this._elements[this._elements.length - 1] ?? null;
+  }
+
+	get startTimeMs(): number {
+    return this.firstElement?.startTimeMs ?? 0;
+  }
+
+	get endTimeMs(): number {
+    return this.lastElement?.endTimeMs ?? 0;
+  }
+
+	get durationMs(): number {
+    return this.endTimeMs - this.startTimeMs;
+  }
+
+  get totalLength(): number {
+    return this.endTimeMs;
+  }
+
+  /**
+   * Removes intersection between elements.
+   */
+  private _fixTimeOffsets(): void {
+    this._elements.sort((a, b) => a.startTimeMs - b.startTimeMs);
+
+    // There is no sense to change offsets when there are 1 element or less.
+    if (this._elements.length <= 1) return;
+
+    // Min starting time of each next element.
+    let nextMinTime = this._elements[0].endTimeMs;
+
+    this._elements.forEach((element, index) => {
+      if (index === 0) return;
+      
+      const difference = element.startTimeMs - nextMinTime;
+      
+      // This means that our current element intersects with previous element.
+      if (difference < 0) element.offsetMs -= difference;
+
+      nextMinTime = element.endTimeMs;
+    });
+  }
+}
