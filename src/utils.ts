deb98147a75578ff7093192f0e078a25b9d34e85
Alexander <sashakostyunin1@gmail.com>
2022-10-02 05:51:16 +0300

    Describe utility methods

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
+import { Ref } from 'react';
+
+/**
+ * @param obj Target object.
+ * @returns Whether this object is iterable or not.
+ */
 export function isIterable(obj: any): boolean {
   if (obj === null || obj === undefined) {
     return false;
@@ -5,6 +11,12 @@ export function isIterable(obj: any): boolean {
   return typeof obj[Symbol.iterator] === 'function';
 }
 
-export function convertToRef(target: any, name: string) {
+/**
+ * Converts object property to a ref.
+ * @param target Target object.
+ * @param name Object property.
+ * @returns Converted ref.
+ */
+export function convertToRef(target: any, name: string): Ref<any> {
   return (e: any) => e && (target[name] = e);
 }
