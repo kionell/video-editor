db017095c1406db642c886d657cb15b8a0fb5b6e
Alexander <sashakostyunin1@gmail.com>
2022-10-03 02:29:08 +0300

    Hide scrollbars when not needed

--- src/components/Containers/ScrollableContainer.tsx
+++ src/components/Containers/ScrollableContainer.tsx
@@ -3,7 +3,11 @@ import { FlexProps, FlexContainer } from './FlexContainer';
 
 const ScrollableContainer: React.FC<FlexProps> = (props: FlexProps) => {
   return (
-    <Scrollbars style={{ width: '100%', height: '100%' }}>
+    <Scrollbars 
+      style={{ width: '100%', height: '100%' }} 
+      hideTracksWhenNotNeeded
+      autoHide 
+    >
       <FlexContainer {...props} />
     </Scrollbars>
   );
