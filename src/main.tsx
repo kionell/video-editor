d3799e9b15e21fd4f9468671e94b2d00d443ddd6
Alexander <sashakostyunin1@gmail.com>
2022-10-03 02:23:53 +0300

    Remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 const element = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(element);
 
-root.render((
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-));
+root.render(<App />);
