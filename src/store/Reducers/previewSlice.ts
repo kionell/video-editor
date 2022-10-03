fe0a112b7d7895e4689c12032074ac574a3ff622
Alexander <sashakostyunin1@gmail.com>
2022-10-03 03:41:02 +0300

    Combine preview & settings slice

--- src/store/Reducers/previewSlice.ts
+++ src/store/Reducers/previewSlice.ts
@@ -1,7 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AspectRatio } from '../../models/Enums/AspectRatio';
 import { IVideoPreview } from '../../models/IVideoPreview';
 
 const initialState: IVideoPreview = {
+  aspectRatio: 1,
+  previewWidth: 1920,
+  previewHeight: 1080,
+  previewFrameRate: 60,
   isPlaying: false,
   isExpanded: false,
 };
@@ -10,6 +15,22 @@ const previewSlice = createSlice({
   name: 'preview',
   initialState,
   reducers: {
+    setAspectRatio(state, action: PayloadAction<AspectRatio>) {
+      state.aspectRatio = action.payload;
+    },
+
+    setPreviewWidth(state, action: PayloadAction<number>) {
+      state.previewWidth = action.payload;
+    },
+   
+    setPreviewHeight(state, action: PayloadAction<number>) {
+      state.previewHeight = action.payload;
+    },
+
+    setPreviewFrameRate(state, action: PayloadAction<number>) {
+      state.previewFrameRate = action.payload;
+    },
+
     setPlaying(state, action: PayloadAction<boolean>) {
       state.isPlaying = action.payload;
     },
@@ -20,5 +41,13 @@ const previewSlice = createSlice({
   }
 });
 
-export const { setPlaying, setExpanded } = previewSlice.actions;
+export const {
+  setAspectRatio,
+  setPreviewWidth,
+  setPreviewHeight,
+  setPreviewFrameRate,
+  setPlaying,
+  setExpanded
+} = previewSlice.actions;
+
 export const previewReducer = previewSlice.reducer;
