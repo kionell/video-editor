b37728db3fe0b1adc7ccec11559a29b3806f2d45
Alexander <sashakostyunin1@gmail.com>
2022-10-03 05:27:16 +0300

    Split all utility methods to a different files

---
+++ src/utils/format.ts
@@ -0,0 +1,26 @@
+/**
+ * Converts raw time value to the readable format.
+ * @param time Target time in seconds.
+ * @returns Time in format H:MM:SS.
+ */
+ export function formatTime(time?: number): string {
+  if (!time) return '0:00';
+  
+  time = Math.round(time);
+
+  const seconds = Math.trunc(time) % 60;
+  const minutes = Math.trunc(time / 60) % 60;
+  const hours = Math.trunc(time / 3600);
+
+  const values = [
+    minutes.toString(),
+    seconds.toString().padStart(2, '0'),
+  ];
+
+  if (hours > 0) {
+    values.unshift(hours.toString());
+    values[1] = values[1].padStart(2, '0');
+  }
+
+  return values.join(':');
+}
---
+++ src/utils/react.ts
@@ -0,0 +1,22 @@
+import { Ref } from 'react';
+
+/**
+ * @param obj Target object.
+ * @returns Whether this object is iterable or not.
+ */
+export function isIterable(obj: any): boolean {
+  if (obj === null || obj === undefined) {
+    return false;
+  }
+  return typeof obj[Symbol.iterator] === 'function';
+}
+
+/**
+ * Converts object property to a ref.
+ * @param target Target object.
+ * @param name Object property.
+ * @returns Converted ref.
+ */
+export function convertToRef(target: any, name: string): Ref<any> {
+  return (e: any) => e && (target[name] = e);
+}
