68c0191579f05ed707c16b2de9d0fff7ae874c0e
Alexander <sashakostyunin1@gmail.com>
2022-11-04 17:24:32 +0200

    Add constant for larger font size

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const LARGE_ICON_SIZE = 30;
 export const NORMAL_ICON_SIZE = 20;
 export const SMALL_ICON_SIZE = 14;
 
+export const LARGER_FONT_SIZE = 30;
 export const LARGE_FONT_SIZE = 24;
 export const NORMAL_FONT_SIZE = 16;
 export const SMALL_FONT_SIZE = 12;
