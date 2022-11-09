77fb44f145a5b78ab261d32eb47577751e1fda40
Alexander <sashakostyunin1@gmail.com>
2022-11-09 19:55:07 +0200

    Fix slider tooltip in firefox

--- src/components/Inputs/Slider.tsx
+++ src/components/Inputs/Slider.tsx
@@ -14,7 +14,7 @@ export interface SliderProps {
   onInput?: FormEventHandler<HTMLInputElement>;
 }
 
-const StyledSliderWrapper = styled.div<SliderProps>`
+const StyledSliderContainer = styled.div<SliderProps>`
   display: flex;
   flex-direction: column;
   align-items: start;
@@ -35,48 +35,16 @@ const StyledSlider = styled.input.attrs({ type: 'range' })<SliderProps>`
   position: relative;
   width: 100%;
 
-  --range: calc(var(--max) - var(--min));
-  --ratio: calc((var(--value) - var(--min)) / var(--range));
-  --sx: calc(0.5 * 10px + var(--ratio) * (100% - 10px));
+  &::-moz-range-track {
+    appearance: none;
+    border-radius: 4px;
+    height: 6px;
+  }
 
   &::-webkit-slider-runnable-track {
     appearance: none;
     border-radius: 4px;
     height: 6px;
-
-    background: ${(props) => {
-      const rangeColor = props.theme.primary.accent;
-      const trackColor = props.theme.input.normal;
-      
-      return css`
-        linear-gradient(${rangeColor}, ${rangeColor}) 0/var(--sx) 100% no-repeat, ${trackColor};
-      `;
-    }};
-  }
-
-  &::-moz-range-track {
-    appearance: none;
-    border-radius: 4px;
-    height: 6px;
-
-    background: ${(props) => {
-      const rangeColor = props.theme.primary.accent;
-      const trackColor = props.theme.input.normal;
-      
-      return css`
-        linear-gradient(${rangeColor}, ${rangeColor}) 0/var(--sx) 100% no-repeat, ${trackColor};
-      `;
-    }};
-  }
-
-  &::-webkit-slider-thumb {
-    appearance: none;
-    width: 12px;
-    height: 12px;
-    border-radius: 50%;
-    outline: none;
-    background: ${(props) => props.theme.primary.accent};
-    translate: 0px -3px;
   }
 
   &::-moz-range-thumb {
@@ -85,10 +53,68 @@ const StyledSlider = styled.input.attrs({ type: 'range' })<SliderProps>`
     height: 12px;
     border-radius: 50%;
     outline: none;
+    border: none;
     background: ${(props) => props.theme.primary.accent};
   }
+  
+  &::-webkit-slider-thumb {
+    appearance: none;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    outline: none;
+    border: none;
+    translate: 0px -3px;
+    background: ${(props) => props.theme.primary.accent};
+  }
+`;
 
-  &:enabled {
+const StyledSliderWrapper = styled.div<SliderProps>`
+  --range: calc(var(--max) - var(--min));
+  --ratio: calc((var(--value) - var(--min)) / var(--range));
+  --sx: calc(0.5 * 10px + var(--ratio) * (100% - 10px));
+
+  height: 0px;
+
+  &:active::before {
+    font-size: 18px;
+    font-family: ${DEFAULT_FONT};
+    user-select: none;
+    background-color: ${(props) => props.theme.primary.normal};
+    color: ${(props) => props.theme.text.lighter};
+    text-align: center;
+    border-radius: 6px;
+    padding: 5px;
+    position: absolute;
+    z-index: 1;
+    left: var(--sx);
+    translate: -50% -125%;
+    content: var(--display);
+  }
+
+  input[type=range]::-moz-range-track {
+    background: ${(props) => {
+      const rangeColor = props.theme.primary.accent;
+      const trackColor = props.theme.input.normal;
+      
+      return css`
+        linear-gradient(${rangeColor}, ${rangeColor}) 0/var(--sx) 100% no-repeat, ${trackColor};
+      `;
+    }};
+  }
+
+  input[type=range]::-webkit-slider-runnable-track {
+    background: ${(props) => {
+      const rangeColor = props.theme.primary.accent;
+      const trackColor = props.theme.input.normal;
+      
+      return css`
+        linear-gradient(${rangeColor}, ${rangeColor}) 0/var(--sx) 100% no-repeat, ${trackColor};
+      `;
+    }};
+  }
+
+  input[type=range]:enabled {
     cursor: pointer;
 
     &:hover {
@@ -97,39 +123,23 @@ const StyledSlider = styled.input.attrs({ type: 'range' })<SliderProps>`
         const normalColor = props.theme.input.normalHover;
 
         return css`
-          &::-webkit-slider-runnable-track {
-            background: linear-gradient(${accentColor}, ${accentColor}) 0/var(--sx) 100% no-repeat, ${normalColor};
-          }
-
           &::-moz-range-track {
             background: linear-gradient(${accentColor}, ${accentColor}) 0/var(--sx) 100% no-repeat, ${normalColor};
           }
 
-          &::-webkit-slider-thumb {
-            background: ${accentColor};
+          &::-webkit-slider-runnable-track {
+            background: linear-gradient(${accentColor}, ${accentColor}) 0/var(--sx) 100% no-repeat, ${normalColor};
           }
 
           &::-moz-range-thumb {
             background: ${accentColor};
           }
+
+          &::-webkit-slider-thumb {
+            background: ${accentColor};
+          }
         `;
       }};
-    }
-
-    &:active::before {
-      font-size: 18px;
-      font-family: ${DEFAULT_FONT};
-      user-select: none;
-      background-color: ${(props) => props.theme.primary.normal};
-      color: ${(props) => props.theme.text.lighter};
-      text-align: center;
-      border-radius: 6px;
-      padding: 5px;
-      position: absolute;
-      z-index: 1;
-      left: var(--sx);
-      translate: -50% -125%;
-      content: var(--display);
     }
   }
 `;
@@ -137,17 +147,19 @@ const StyledSlider = styled.input.attrs({ type: 'range' })<SliderProps>`
 const Slider: React.FC<SliderProps> = (props: SliderProps) => {
   const { showLabel, label, minValue, maxValue, step } = props;
 
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const sliderRef = useRef<HTMLInputElement>(null);
 
   const onInputListener = () => {
-    if (!sliderRef.current) return;
+    if (!sliderRef.current || !wrapperRef.current) return;
 
-    const el = sliderRef.current;
+    const wrapper = wrapperRef.current;
+    const slider = sliderRef.current;
 
-    el.style.setProperty('--min', el.min);
-    el.style.setProperty('--max', el.max);
-    el.style.setProperty('--value', el.value);
-    el.style.setProperty('--display', `"${el.value}"`);
+    wrapper.style.setProperty('--min', slider.min);
+    wrapper.style.setProperty('--max', slider.max);
+    wrapper.style.setProperty('--value', slider.value);
+    wrapper.style.setProperty('--display', `"${slider.value}"`);
   };
 
   useEffect(() => {
@@ -163,20 +175,22 @@ const Slider: React.FC<SliderProps> = (props: SliderProps) => {
   }, []);
 
   return (
-    <StyledSliderWrapper {...props}>
+    <StyledSliderContainer {...props}>
       <Text 
         visible={showLabel} 
         text={label} 
         size={NORMAL_FONT_SIZE}
       />
-      <StyledSlider 
-        {...props} 
-        ref={sliderRef}
-        min={minValue}
-        max={maxValue}
-        step={step}
-      />
-    </StyledSliderWrapper>
+      <StyledSliderWrapper ref={wrapperRef}>
+        <StyledSlider 
+          {...props} 
+          ref={sliderRef}
+          min={minValue}
+          max={maxValue}
+          step={step}
+        />
+      </StyledSliderWrapper>
+    </StyledSliderContainer>
   );
 };
 
