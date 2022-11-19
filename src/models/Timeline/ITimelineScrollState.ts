fb3e48b9310cdc5205b8f995c02326d3464a2d0c
Alexander <sashakostyunin1@gmail.com>
2022-11-19 18:49:08 +0200

    Add timeline scroll state interface

---
+++ src/models/Timeline/ITimelineScrollState.ts
@@ -0,0 +1,14 @@
+/**
+ * A timeline scroll state.
+ */
+export interface ITimelineScrollState {
+  /**
+   * Timeline scroll state by X-axis.
+   */
+  left: number;
+
+  /**
+   * Timeline scroll state by Y-axis.
+   */
+  top: number;
+}
