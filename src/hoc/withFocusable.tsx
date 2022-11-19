c35846dc154005cee1e9fa4f56d4001abb4ce3db
Alexander <sashakostyunin1@gmail.com>
2022-11-19 15:27:13 +0200

    Rework focusable HOC

--- src/hoc/withFocusable.tsx
+++ src/hoc/withFocusable.tsx
@@ -1,43 +1,99 @@
-import { ForwardedRef, forwardRef, useEffect, useState } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
+import Sister, { SisterEventListener } from 'sister';
+
+type FocusableEventType = 'focus' | 'blur';
+
+export interface FocusableState {
+  type: FocusableEventType;
+  target: HTMLElement;
+  isFocused: boolean;
+}
+
+export type FocusableCallback = (state: FocusableState) => void;
+
+interface FocusableProps {
+  focusCallback?: FocusableCallback;
+  blurCallback?: FocusableCallback;
+}
 
 export const withFocusable = <T, >(Component: React.FC<T>) => {
-  const FocusedComponent = forwardRef<HTMLElement, T>((
-    props: T,
-    ref: ForwardedRef<HTMLElement>,
-  ) => {
-    const [selected, setSelected] = useState(false);
-
-    const unfocusElement = (event: MouseEvent) => {
-      if (ref instanceof Function || !ref?.current) return;
-
-      const targetPath = event.composedPath();
-
-      if (event.target !== ref.current && !targetPath?.includes(ref.current)) {
-        setSelected(false);
-      }
-    };
-
-    const focusElement = () => setSelected(true);
+  const FocusedComponent = forwardRef<HTMLElement, T & FocusableProps>((props, ref) => {
+    const [focused, setFocused] = useState(false);
 
     useEffect(() => {
       if (ref instanceof Function || !ref?.current) return;
 
-      ref.current.addEventListener('mousedown', focusElement);
-      document.addEventListener('mousedown', unfocusElement);
+      const makeFocusable = (element: HTMLElement, props: T & FocusableProps) => {
+        const emitter = new Sister();
 
-      return () => {
-        document.removeEventListener('mousedown', unfocusElement);
-        ref.current?.removeEventListener('mousedown', focusElement);
+        const focusElement = (event: MouseEvent) => {
+          event.stopPropagation();
+
+          if (focused) return;
+
+          setFocused(true);
+
+          document.addEventListener('mousedown', unfocusElement);
+
+          emitter.trigger<FocusableEventType, FocusableState>('focus', {
+            type: 'focus',
+            target: element,
+            isFocused: true,
+          });
+        };
+
+        const unfocusElement = (event: MouseEvent) => {
+          event.stopPropagation();
+
+          const targetPath = event.composedPath();
+
+          if (event.target !== element && !targetPath?.includes(element)) {
+            setFocused(false);
+
+            document.removeEventListener('mousedown', unfocusElement);
+
+            emitter.trigger<FocusableEventType, FocusableState>('blur', {
+              type: 'blur',
+              target: element,
+              isFocused: false,
+            });
+          }
+        };
+
+        element.addEventListener('mousedown', focusElement);
+
+        let focusCallbackListener: SisterEventListener | null = null;
+        let blurCallbackListener: SisterEventListener | null = null;
+
+        if (props.focusCallback) {
+          focusCallbackListener = emitter.on('focus', props.focusCallback);
+        }
+
+        if (props.blurCallback) {
+          blurCallbackListener = emitter.on('blur', props.blurCallback);
+        }
+
+        return () => {
+          element.removeEventListener('mousedown', focusElement);
+
+          if (focusCallbackListener) {
+            emitter.off(focusCallbackListener);
+          }
+
+          if (blurCallbackListener) {
+            emitter.off(blurCallbackListener);
+          }
+        };
       };
+
+      return makeFocusable(ref.current, props);
     }, []);
 
-    return (
-      <Component
-        ref={ref}
-        className={selected ? 'focused' : ''}
-        {...props}
-      />
-    );
+    return <Component
+      {...props}
+      ref={ref}
+      className={focused ? 'focused' : ''}
+    />;
   });
 
   FocusedComponent.displayName = 'Focused Component';
