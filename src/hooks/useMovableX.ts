2538fbddb685de996d31ee50562d22c31f065735
Alexander <sashakostyunin1@gmail.com>
2022-11-21 19:20:39 +0200

    Allow moving with touch events

--- src/hooks/useMovableX.ts
+++ src/hooks/useMovableX.ts
@@ -1,29 +1,48 @@
 import { useEffect, Ref } from 'react';
+import {
+  createPositionTracker,
+  IPositionTrackerState,
+  PositionEvent,
+} from '../core/Utils/Position';
 
 export function useMovableX(ref: Ref<HTMLElement>): void {
   const makeMovableX = (element: HTMLElement) => {
-    let offsetX = 0;
+    const tracker = createPositionTracker();
 
-    const startMoving = (event: MouseEvent) => {
-      offsetX = element.offsetLeft - event.pageX;
+    let position: IPositionTrackerState;
+    let startOffsetX = 0;
+
+    const startMoving = (event: PositionEvent) => {
+      position = tracker.start(event);
+
+      startOffsetX = element.offsetLeft - position.pageX;
+
       element.style.position = 'absolute';
 
+      document.addEventListener('touchmove', moveElement);
+      document.addEventListener('touchend', stopMoving);
       document.addEventListener('mousemove', moveElement);
       document.addEventListener('mouseup', stopMoving);
     };
 
-    const moveElement = (event: MouseEvent) => {
-      element.style.left = Math.max(0, (event.pageX + offsetX)) + 'px';
+    const moveElement = (event: PositionEvent) => {
+      position = tracker.update(event);
+
+      element.style.left = position.pageX + startOffsetX + 'px';
     };
 
     const stopMoving = () => {
+      document.removeEventListener('touchmove', moveElement);
+      document.removeEventListener('touchend', stopMoving);
       document.removeEventListener('mousemove', moveElement);
       document.removeEventListener('mouseup', stopMoving);
     };
 
+    element.addEventListener('touchstart', startMoving);
     element.addEventListener('mousedown', startMoving);
 
     return () => {
+      element.removeEventListener('touchstart', startMoving);
       element.removeEventListener('mousedown', startMoving);
 
       stopMoving();
