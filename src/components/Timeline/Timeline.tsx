df351eb7d2500beb262541835372d16bad845af0
Alexander <sashakostyunin1@gmail.com>
2022-11-28 17:19:01 +0200

    Make sidebar and timeline resizable

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,23 +1,32 @@
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { useAppSelector } from '../../hooks/useAppSelector';
+import { useResizable } from '../../hooks/useResizable';
+import { ResizerLine } from '../Containers/Resizer';
 import { TimelineTools } from './TimelineTools';
 import { TimelineTrackpad } from './TimelineTrackpad';
 import { TimelineTrackpadPlaceholder } from './TimelineTrackpadPlaceholder';
 
 const StyledTimeline = styled.div`
   width: 100%;
-  height: 300px;
+  height: 350px;
+  min-height: 50px;
   display: flex;
   flex-direction: column;
   position: relative;
+  user-select: none;
   background: ${(props) => props.theme.primary.surface};
 `;
 
 const Timeline: React.FC = () => {
   const timeline = useAppSelector((state) => state.timeline);
+  const timelineRef = useRef();
+
+  useResizable(timelineRef);
 
   return (
-    <StyledTimeline>
+    <StyledTimeline ref={timelineRef}>
+      <ResizerLine direction='top'/>
       <TimelineTools />
       {
         timeline.tracks.length > 0
