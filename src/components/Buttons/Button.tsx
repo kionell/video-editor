592013e1728ab0a63783f1da8b3d87a936de18c1
Alexander <sashakostyunin1@gmail.com>
2022-11-29 09:24:54 +0200

    Add new button props to set full width and height

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -5,7 +5,9 @@ import { IconType, IconSize } from '../Icon';
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   visible?: boolean;
   disabled?: boolean;
+  fullWidth?: boolean;
   width?: number;
+  fullHeight?: boolean;
   height?: number;
   paddingHorizontal?: number;
   paddingVertical?: number;
@@ -29,12 +31,20 @@ const StyledBaseButton = styled.button<ButtonProps>`
   transition: background 100ms;
 
   ${(props) => {
+    if (props.fullWidth) {
+      return css`width: 100%;`;
+    }
+
     return props.width && css`
       width: ${props.width}px;
     `;
   }}
 
   ${(props) => {
+    if (props.fullHeight) {
+      return css`height: 100%;`;
+    }
+
     return props.height && css`
       height: ${props.height}px;
     `;
@@ -75,13 +85,14 @@ StyledBaseButton.defaultProps = {
   visible: true,
   disabled: false,
   toggled: false,
+  fullWidth: false,
+  fullHeight: false,
   height: 40,
   paddingHorizontal: 12,
   paddingVertical: 7,
   showIcon: true,
   iconType: 'Check',
   iconSize: 'Normal',
-  showLabel: true,
   label: 'Button',
 };
 
