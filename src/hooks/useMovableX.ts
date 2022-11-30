17d56f0e216c1f472eb305802e814aaa89088df8
Alexander <sashakostyunin1@gmail.com>
2022-11-30 05:01:03 +0200

    Add event listeners for movable hook

--- src/hooks/useMovableX.ts
+++ src/hooks/useMovableX.ts
@@ -1,13 +1,31 @@
 import { useEffect, Ref } from 'react';
+import Sister, { SisterEventListener } from 'sister';
 import {
   createPositionTracker,
   IPositionTrackerState,
   PositionEvent,
 } from '../core/Utils/Position';
 
-export function useMovableX(ref: Ref<HTMLElement>): void {
-  const makeMovableX = (element: HTMLElement) => {
+type MovableXEventType = 'start' | 'move' | 'end';
+
+export interface MovableXState {
+  type: MovableXEventType;
+  target: HTMLElement;
+  position: IPositionTrackerState,
+}
+
+export type MovableXCallback = (state: MovableXState) => void;
+
+export interface MovableXProps {
+  startMoveCallback?: MovableXCallback;
+  moveCallback?: MovableXCallback;
+  stopMoveCallback?: MovableXCallback;
+}
+
+export function useMovableX(ref: Ref<HTMLElement>, props?: MovableXProps): void {
+  const makeMovableX = (element: HTMLElement, props?: MovableXProps) => {
     const tracker = createPositionTracker();
+    const emitter = new Sister();
 
     let position: IPositionTrackerState;
     let startOffsetX = 0;
@@ -23,12 +41,24 @@ export function useMovableX(ref: Ref<HTMLElement>): void {
       document.addEventListener('touchend', stopMoving);
       document.addEventListener('mousemove', moveElement);
       document.addEventListener('mouseup', stopMoving);
+
+      emitter.trigger('startmove', {
+        type: 'startmove',
+        target: element,
+        position,
+      });
     };
 
     const moveElement = (event: PositionEvent) => {
       position = tracker.update(event);
 
       element.style.left = position.pageX + startOffsetX + 'px';
+
+      emitter.trigger('move', {
+        type: 'move',
+        target: element,
+        position,
+      });
     };
 
     const stopMoving = () => {
@@ -36,12 +66,46 @@ export function useMovableX(ref: Ref<HTMLElement>): void {
       document.removeEventListener('touchend', stopMoving);
       document.removeEventListener('mousemove', moveElement);
       document.removeEventListener('mouseup', stopMoving);
+
+      emitter.trigger('stopmove', {
+        type: 'stopmove',
+        target: element,
+        position,
+      });
     };
+
+    let startCallbackListener: SisterEventListener | null = null;
+    let moveCallbackListener: SisterEventListener | null = null;
+    let stopCallbackListener: SisterEventListener | null = null;
+
+    if (props?.startMoveCallback) {
+      startCallbackListener = emitter.on('start', props.startMoveCallback);
+    }
+
+    if (props?.moveCallback) {
+      moveCallbackListener = emitter.on('move', props.moveCallback);
+    }
+
+    if (props?.stopMoveCallback) {
+      stopCallbackListener = emitter.on('end', props.stopMoveCallback);
+    }
 
     element.addEventListener('touchstart', startMoving);
     element.addEventListener('mousedown', startMoving);
 
     return () => {
+      if (startCallbackListener) {
+        emitter.off(startCallbackListener);
+      }
+
+      if (moveCallbackListener) {
+        emitter.off(moveCallbackListener);
+      }
+
+      if (stopCallbackListener) {
+        emitter.off(stopCallbackListener);
+      }
+
       element.removeEventListener('touchstart', startMoving);
       element.removeEventListener('mousedown', startMoving);
 
@@ -52,6 +116,6 @@ export function useMovableX(ref: Ref<HTMLElement>): void {
   useEffect(() => {
     if (ref instanceof Function || !ref?.current) return;
 
-    return makeMovableX(ref.current);
+    return makeMovableX(ref.current, props);
   }, []);
 }
