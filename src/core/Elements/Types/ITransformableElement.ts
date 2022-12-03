672d6a2e122768ec77bcb64057da6a3fc0a9b6d2
Alexander <sashakostyunin1@gmail.com>
2022-12-03 13:03:34 +0200

    Remove bounds and scale from transformable element

--- src/core/Elements/Types/ITransformableElement.ts
+++ src/core/Elements/Types/ITransformableElement.ts
@@ -1,28 +1,11 @@
-import { VisualBounds } from '../../Types/VisualBounds';
-
 /**
  * An element that can be transformed.
  */
 export interface ITransformableElement {
-	/**
-   * Element bounds.
-   */
-  bounds: VisualBounds;
-
   /**
    * Element rotation in degrees.
    */
   rotation: number;
-
-  /**
-   * Element scale.
-   */
-  scale: number;
-
-  /**
-   * Element zoom.
-   */
-	zoom: number;
 
   /**
    * Whether this element is fliped horizontally or not.
