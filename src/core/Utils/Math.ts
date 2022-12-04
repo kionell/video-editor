f1dc4a2127b3c05a79ed3e3621000115753437c2
Alexander <sashakostyunin1@gmail.com>
2022-12-04 13:33:55 +0200

    Add map function to math utils

--- src/core/Utils/Math.ts
+++ src/core/Utils/Math.ts
@@ -1,3 +1,10 @@
 export function clamp(value: number, min: number, max: number) {
   return Math.max(min, Math.min(value, max));
 }
+
+export function map(value: number, from1: number, to1: number, from2: number, to2: number): number {
+  if (value < from1) return from2;
+  if (value >= to1) return to2;
+
+  return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+}
