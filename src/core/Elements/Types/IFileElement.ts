8e23a371ffd3d3f11a38942722191e3a858cb5e1
Alexander <sashakostyunin1@gmail.com>
2022-12-06 12:23:34 +0200

    Add default file element generic type

--- src/core/Elements/Types/IFileElement.ts
+++ src/core/Elements/Types/IFileElement.ts
@@ -3,7 +3,7 @@ import { UploadedFile } from '../../Files/UploadedFile';
 /**
  * An element that is loaded from a file.
  */
-export interface IFileElement<T extends UploadedFile> {
+export interface IFileElement<T extends UploadedFile = UploadedFile> {
   /**
    * Uploaded file with loaded HTML element.
    */
